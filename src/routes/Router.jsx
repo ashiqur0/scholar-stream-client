2b970a4b143b22180e5555d140d5f6d70c800a58
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-08 21:53:04 +0600

    Added RootLayout Component to Router Component as Root Path

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
+import RootLayout from '../layout/RootLayout';
 
 const Router = createBrowserRouter([
     {
         path: '/',
-        element: <h1>From React Router</h1>
+        Component: RootLayout
     }
 ])
 
