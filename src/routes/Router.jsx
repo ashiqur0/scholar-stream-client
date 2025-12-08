36236f3b2224e4a084ca7b63002d6f7f5ab2d853
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-08 21:56:32 +0600

    Added Home Page Component As Children Component of RootLayout in Router Component

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
 import RootLayout from '../layout/RootLayout';
+import Home from '../pages/Home/Home/Home';
 
 const Router = createBrowserRouter([
     {
         path: '/',
-        Component: RootLayout
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            }
+        ]
     }
 ])
 
