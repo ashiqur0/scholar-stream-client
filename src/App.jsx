20738b8c5bcabdbfbd673e1a0ebc83b1ca366149
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-08 21:37:14 +0600

    Installed Daisy UI To The Projects

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
     <div>
       <h1 className='text-3xl'>Lorem ipsum dolor sit amet.</h1>
       <h1 className='text-red-500'>Lorem ipsum dolor sit amet.</h1>
+
+      <button className='btn btn-primary'>Button</button>
     </div>
   );
 };
