c2424c55c2eb8d7a12b725a72e5f7b6062011b46
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-09 21:16:12 +0600

    Added All Scholarships Component To Router Component

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter } from "react-router";
 import RootLayout from '../layout/RootLayout';
 import Home from '../pages/Home/Home/Home';
+import AllScholarships from '../pages/All-Scholarships/AllScholarships';
 
 const Router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const Router = createBrowserRouter([
             {
                 index: true,
                 Component: Home
+            },
+            {
+                path: '/all-scholarships',
+                Component: AllScholarships
             }
         ]
     }
