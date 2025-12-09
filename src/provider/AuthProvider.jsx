4867b107c394841de344c4d37b2c775b413ebd2b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-09 20:15:42 +0600

    Set Theme Toggle Functionalities in AuthProvider Component

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthContext from '../context/AuthContext';
 
 const AuthProvider = ({ children }) => {
 
-    const authInfo = {
+    // Theme related functionalities
+    const [toggle, setToggle] = useState(true);
+    const [theme, setTheme] = useState('dark');
+    const changeTheme = () => {
+        toggle ? setTheme('dark') : setTheme('light');
+        setToggle(!toggle);
 
+        document.documentElement.setAttribute("data-theme", theme);
+    }
+
+    const authInfo = {
+        changeTheme,
     }
 
     return <AuthContext value={authInfo}>{children}</AuthContext>;
