024fc302f064e4f7fd16a1dd81c36ba9d030c66e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-09 21:14:29 +0600

    Create All Scholarships Component

---
+++ src/pages/All-Scholarships/AllScholarships.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllScholarships = () => {
+    return (
+        <div>
+            <h1>All Scholarships Page</h1>
+        </div>
+    );
+};
+
+export default AllScholarships;
