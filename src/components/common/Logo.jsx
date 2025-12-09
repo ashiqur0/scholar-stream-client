50b73398509cb2f2d7dac372279d448c30d9925a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-09 21:25:20 +0600

    Update Logo Component as Clickable Link of Home Page

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logoImg from '../../assets/logo.png'
+import { Link } from 'react-router';
 
 const Logo = () => {
     return (
-        <div className='flex justify-center items-end gap-3'>
+        <Link to={'/'} className='flex justify-center items-end gap-3'>
             <img src={logoImg} className='w-14 h-auto' alt='logo image'></img>
             <h1 className='text-2xl font-bold'>Scholars Stream</h1>
-        </div>
+        </Link>
     );
 };
 
