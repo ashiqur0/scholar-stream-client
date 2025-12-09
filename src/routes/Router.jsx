a948ce7a6d767be6ea9577c5e11fadcb3e023db3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-09 21:52:27 +0600

    Added Authlayout Component to Router Component

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,7 @@ import RootLayout from '../layout/RootLayout';
 import Home from '../pages/Root/Home/Home/Home';
 import AllScholarships from '../pages/Root/All-Scholarships/AllScholarships';
 import ScholarshipDetails from '../pages/Root/Scholaships-Details/ScholarshipDetails';
+import AuthLayout from '../layout/AuthLayout';
 
 const Router = createBrowserRouter([
     {
@@ -24,7 +25,7 @@ const Router = createBrowserRouter([
             },
             {
                 path: '/',
-                Component: 
+                Component: AuthLayout
             }
         ]
     }
