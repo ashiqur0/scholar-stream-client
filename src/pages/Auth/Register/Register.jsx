55f1864493466cd0572f56777d41eac51340afae
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-09 21:45:33 +0600

    Create Register Component for User Registration

---
+++ src/pages/Auth/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Register Your Account</h1>
+        </div>
+    );
+};
+
+export default Register;
