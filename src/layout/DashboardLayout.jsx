d172214d9fb05acce7d69129ab418c63d7f47612
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 19:23:41 +0600

    Create Dashboard Layout Component

---
+++ src/layout/DashboardLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardLayout = () => {
+
+    return (
+        <div>
+            <h1>Dashboard Layout Component</h1>
+        </div>
+    );
+};
+
+export default DashboardLayout;
