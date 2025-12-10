7f041437539e77fd22ec63beb091335666dd0bb2
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 21:29:20 +0600

    Added ModeratorDashboardHome Component to DashboardHome Component With Conditional Rendering

--- src/pages/Dashboard/Home/DashboardHome.jsx
+++ src/pages/Dashboard/Home/DashboardHome.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useRole from '../../../hooks/useRole';
 import Loading from '../../../components/common/Loading';
 import AdminDashboardHome from './AdminDashboardHome';
+import ModeratorDashboardHome from './ModeratorDashboardHome';
 
 const DashboardHome = () => {
 
@@ -9,6 +10,7 @@ const DashboardHome = () => {
 
     if (roleLoading) return <Loading />
     if (role === 'admin') return <AdminDashboardHome />
+    else if (role === 'moderator') return <ModeratorDashboardHome />
 
     return ;
 };
