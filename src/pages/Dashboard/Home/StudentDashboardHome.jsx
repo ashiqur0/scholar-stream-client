3a1d59d17c6189846f50dc645f888add58bd85b5
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 21:30:45 +0600

    Create StudentDashboardHome Component

---
+++ src/pages/Dashboard/Home/StudentDashboardHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentDashboardHome = () => {
+    return (
+        <div>
+            <h1>Student Dashboard Home</h1>
+        </div>
+    );
+};
+
+export default StudentDashboardHome;
