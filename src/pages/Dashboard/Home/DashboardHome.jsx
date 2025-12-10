d1867c8a5f700a28125245f21f7be1c2fc32e8c9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 21:26:32 +0600

    Added AdminDashboardHome Component to DashboardHome Component With Conditional Rendering

--- src/pages/Dashboard/Home/DashboardHome.jsx
+++ src/pages/Dashboard/Home/DashboardHome.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import useRole from '../../../hooks/useRole';
+import Loading from '../../../components/common/Loading';
+import AdminDashboardHome from './AdminDashboardHome';
 
 const DashboardHome = () => {
+
+    const { role, roleLoading } = useRole();
+
+    if (roleLoading) return <Loading />
+    if (role === 'admin') return <AdminDashboardHome />
+
     return ;
 };
 
