eb71e16bb0b8f009b843b595a713ea1a4cdd2253
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 21:05:07 +0600

    Update main.jsx With Set QueryClientProvider As Parent Component of AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import Router from './routes/Router.jsx'
 import AuthProvider from './provider/AuthProvider.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={Router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={Router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
