05ea232cee9dab0a3603dc401615af7ee0eb2306
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 07:00:45 +0600

    Update PageNotFound Component

--- src/components/common/PageNotFound.jsx
+++ src/components/common/PageNotFound.jsx
@@ -4,7 +4,7 @@ import errorImg from '../../assets/error/error.jpg'
 const PageNotFound = () => {
     return (
         <div>
-            <div className='bg-slate-900 w-full md:mx-auto flex justify-center items-center py-20 px-10'>
+            <div className='bg-slate-900 w-full md:mx-auto flex justify-center items-center py-20 px-10 min-h-screen'>
                 <img src={errorImg} alt="" />
             </div>
         </div>
