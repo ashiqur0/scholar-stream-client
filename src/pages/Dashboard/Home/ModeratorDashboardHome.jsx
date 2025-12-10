5acbed27c1b1f2f899f178ad512253aa0bbe4496
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 21:28:10 +0600

    Create ModeratorDashboardHome Component

---
+++ src/pages/Dashboard/Home/ModeratorDashboardHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ModeratorDashboardHome = () => {
+    return (
+        <div>
+            <h1>Moderator Dashboard Home Page</h1>
+        </div>
+    );
+};
+
+export default ModeratorDashboardHome;
