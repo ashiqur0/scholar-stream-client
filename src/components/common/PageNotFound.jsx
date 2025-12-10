d081360cd29d1849e92e4e9f759568c88bbcb0f7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 06:57:59 +0600

    Create PageNotFound Component

---
+++ src/components/common/PageNotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import errorImg from '../../assets/error/error.jpg'
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <div className='bg-slate-900 w-full md:mx-auto flex justify-center items-center py-20 px-10'>
+                <img src={errorImg} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default PageNotFound;
