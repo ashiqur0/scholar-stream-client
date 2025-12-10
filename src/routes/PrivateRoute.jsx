e8cb0a4833bf98606414d056e11bb9765243eb1d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-10 07:40:16 +0600

    Create PrivateRoute Component To Protect Client Side Pages

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import useAuth from '../hooks/useAuth';
+import { Navigate, useLocation } from 'react-router';
+import Loading from '../components/common/Loading';
+
+const PrivateRoute = ({ children }) => {
+
+    const { loading, user } = useAuth();
+    const location = useLocation();
+
+    if (loading) return <Loading />
+    if (!user) <Navigate to={"/login"} state={location.pathname}></Navigate>
+
+    return children;
+};
+
+export default PrivateRoute;
