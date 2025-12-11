3d3260e877e2e526dbaa8009eb088986cb65f9c9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 17:57:59 +0600

    Create Analytics Page For Admin

---
+++ src/pages/Dashboard/AdminDashboard/Analytics/Analytics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Analytics = () => {
+    return (
+        <div>
+            <h1>Analytics Page For Admin</h1>
+        </div>
+    );
+};
+
+export default Analytics;
