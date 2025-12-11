57c5a52a63ba874bccd1b1664a84191a86bea7e8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 18:05:12 +0600

    Create ManageAppliedApplications Page

---
+++ src/pages/Dashboard/ModeratorDashboard/ManageAppliedApplications/ManageAppliedApplications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageAppliedApplications = () => {
+    return (
+        <div>
+            <h1>Manage Applied Applications Page</h1>
+        </div>
+    );
+};
+
+export default ManageAppliedApplications;
