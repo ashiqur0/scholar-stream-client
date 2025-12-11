e8137f882ca7fb550968e55dda381754122c4f88
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 12:22:37 +0600

    Added TopScholarship Component To Main Home page

--- src/pages/Root/Home/Home/Home.jsx
+++ src/pages/Root/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import TopScholarship from '../Top-Scholarship/TopScholarship';
 
 const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
             <Banner />
+            <TopScholarship />
         </div>
     );
 };
