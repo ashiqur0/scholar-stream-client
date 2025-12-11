4b1e66e9b6859c1d3d1bdf23ae37a2cf8197ad34
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 17:19:44 +0600

    Update Logo Component

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logoImg from '../../assets/logo.png'
+// import logoImg from '../../assets/logo.png'
 import { Link } from 'react-router';
+import { GiGraduateCap } from "react-icons/gi";
 
 const Logo = () => {
     return (
         <Link to={'/'} className='flex justify-center items-end gap-3'>
-            <img src={logoImg} className='w-14 h-auto' alt='logo image'></img>
+            {/* <img src={logoImg} className='w-14 h-auto' alt='logo image'></img> */}
+            <GiGraduateCap size={40}/>
             <h1 className='text-2xl font-bold'>Scholars Stream</h1>
         </Link>
     );
