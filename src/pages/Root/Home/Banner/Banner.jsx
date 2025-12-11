b1f9370b01453aa53368d672e35e89679dae1028
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 11:58:37 +0600

    Create Banner Component For Home Page

---
+++ src/pages/Root/Home/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h1>Banner Page For Main Home Page</h1>
+        </div>
+    );
+};
+
+export default Banner;
