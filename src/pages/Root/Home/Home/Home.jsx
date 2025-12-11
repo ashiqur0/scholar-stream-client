ec545cf95f83156a0190a874b01bc822a8fb4715
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 12:32:36 +0600

    Added FAQ Section To Main Home Page

--- src/pages/Root/Home/Home/Home.jsx
+++ src/pages/Root/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import TopScholarship from '../Top-Scholarship/TopScholarship';
 import Stories from '../Stories/Stories';
+import FAQ from '../FAQ/FAQ';
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner />
             <TopScholarship />
             <Stories />
+            <FAQ />
         </div>
     );
 };
