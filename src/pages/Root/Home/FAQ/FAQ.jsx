ddec53ad0d4b287afd37ba3a80d3d60e5d932b74
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 12:30:47 +0600

    Create FAQ Section For Main Home Page

---
+++ src/pages/Root/Home/FAQ/FAQ.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FAQ = () => {
+    return (
+        <div className='md:max-w-7xl md:mx-auto p-4'>
+            <h1>FAQ Section For Main Home Page</h1>
+        </div>
+    );
+};
+
+export default FAQ;
