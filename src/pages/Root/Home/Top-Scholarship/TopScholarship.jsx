b64d9f5ceaf2376ed5cbe024ed60932ecc202192
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 12:21:54 +0600

    Create TopScholarship Component To Display in Main Component

---
+++ src/pages/Root/Home/Top-Scholarship/TopScholarship.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopScholarship = () => {
+    return (
+        <div className='md:max-w-7xl md:mx-auto p-4'>
+            <h1>Top Scholarship Component For Main Home Page</h1>
+        </div>
+    );
+};
+
+export default TopScholarship;
