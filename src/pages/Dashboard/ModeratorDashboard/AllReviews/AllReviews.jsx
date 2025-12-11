809414300e71b7db3cbfd3376d824cc65f411290
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 19:03:56 +0600

    Create AllReviews Page For Moderator

---
+++ src/pages/Dashboard/ModeratorDashboard/AllReviews/AllReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllReviews = () => {
+    return (
+        <div>
+            <h1>All Reviews Page For Moderator</h1>
+        </div>
+    );
+};
+
+export default AllReviews;
