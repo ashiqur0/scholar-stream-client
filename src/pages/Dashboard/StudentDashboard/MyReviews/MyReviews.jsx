33af19c376814f9e50fbc800485028a4eb3ab23a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 19:54:03 +0600

    Create My Reviews Page For Students

---
+++ src/pages/Dashboard/StudentDashboard/MyReviews/MyReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyReviews = () => {
+    return (
+        <div>
+            <h1>My Review Page For Students</h1>
+        </div>
+    );
+};
+
+export default MyReviews;
