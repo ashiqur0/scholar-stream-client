c71323590d035259db89edb6d206bc5ebaa083d8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 17:04:58 +0600

    Create Manage Scholarship Page For Admin

---
+++ src/pages/Dashboard/AdminDashboard/ManageScholarship/ManageScholarship.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageScholarship = () => {
+    return (
+        <div>
+            <h1>Manage Scholarship Page</h1>
+        </div>
+    );
+};
+
+export default ManageScholarship;
