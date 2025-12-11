e6be265f308ef01360a9e35769b62436ba816fc6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 12:27:24 +0600

    Added Stories Component To Main Home Page

--- src/pages/Root/Home/Home/Home.jsx
+++ src/pages/Root/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import TopScholarship from '../Top-Scholarship/TopScholarship';
+import Stories from '../Stories/Stories';
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <h1>Home Page</h1>
             <Banner />
             <TopScholarship />
+            <Stories />
         </div>
     );
 };
