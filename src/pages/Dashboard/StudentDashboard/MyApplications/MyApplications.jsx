aa78177b0e1795c0f5073e23b7bdec4a940c669b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 19:11:53 +0600

    Create My Applications Page For Students

---
+++ src/pages/Dashboard/StudentDashboard/MyApplications/MyApplications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyApplications = () => {
+    return (
+        <div>
+            <h1>My Applications Page For Student</h1>
+        </div>
+    );
+};
+
+export default MyApplications;
