0cd76e1a626ed6e081b0211822c7e4da33691ff3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-11 12:26:19 +0600

    Create Stories Component For Main Home Page

---
+++ src/pages/Root/Home/Stories/Stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stories = () => {
+    return (
+        <div>
+            <h1>Stories Component for Main Home Page</h1>
+        </div>
+    );
+};
+
+export default Stories;
