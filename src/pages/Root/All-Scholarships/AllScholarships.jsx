6a2f70636293ad47d9a6ce97872a94f19baacf8f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-12 12:40:22 +0600

    Update AllScholarship Page

--- src/pages/Root/All-Scholarships/AllScholarships.jsx
+++ src/pages/Root/All-Scholarships/AllScholarships.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import useAxios from '../../../hooks/useAxios';
+import { useQuery } from '@tanstack/react-query';
+import Scholarship from '../../../components/Scholarship';
 
 const AllScholarships = () => {
+
+    const axios = useAxios();
+
+    const { data: scholarships = [] } = useQuery({
+        queryKey: ['riders', 'pending'],
+        queryFn: async () => {
+            const res = await axios.get('/scholarship');
+            return res.data;
+        }
+    })
+
     return (
-        <div>
-            <h1>All Scholarships Page</h1>
+        <div className='my-6 md:max-w-7xl md:mx-auto p-4'>
+            <h1 className='md:text-3xl text-2xl font-semibold mb-5'>All Scholarship({scholarships.length})</h1>
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                {
+                    scholarships.map(scholarship => <Scholarship key={scholarship._id} scholarship={scholarship}></Scholarship>)
+                }
+            </div>
         </div>
     );
 };
