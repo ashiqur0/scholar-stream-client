0b08607e0b5f7415725530a91858e57be421a3ba
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-13 23:22:46 +0600

    Update Title of Analytics Page

--- src/pages/Dashboard/AdminDashboard/Analytics/Analytics.jsx
+++ src/pages/Dashboard/AdminDashboard/Analytics/Analytics.jsx
@@ -23,7 +23,7 @@ const Analytics = () => {
 
     return (
         <div className='md:max-w-7xl md:mx-auto p-4'>
-            <h1 className='text-2xl font-bold'>Analytics</h1>
+            <h1 className='text-2xl font-bold'>Analytics of Application Status</h1>
 
             <div className="stats shadow">
                 {
