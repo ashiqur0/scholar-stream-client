2dd665c39d609b5b1a6a6db1a690a5b76e080974
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-13 19:56:07 +0600

    Update MyProfile Page

--- src/pages/Dashboard/Common/MyProfile/MyProfile.jsx
+++ src/pages/Dashboard/Common/MyProfile/MyProfile.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import useRole from '../../../../hooks/useRole';
+import uesAuth from '../../../../hooks/useAuth';
 
 const MyProfile = () => {
 
     const { role } = useRole();
+    const { user } = uesAuth();
 
     return (
-        <div>
-            <h1>My Profile Page for {role}</h1>
+        <div className='md:max-w-7xl md:mx-auto p-4'>
+            <h1 className='text-2xl font-bold mb-10'>My Profile Page for {role}</h1>
+
+            <div className='flex justify-center items-center gap-5'>
+                <img className='w-20 h-auto' src={user.photoURL} alt={user.displayName} />
+                <div className='space-y-3'>
+                    <h2>Name: {user.displayName}</h2>
+                    <p>Role: {role}</p>
+                </div>
+            </div>
         </div>
     );
 };
