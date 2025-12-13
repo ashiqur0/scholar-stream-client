4eb7c5058c5f043050a2fe4e68268190abfb4f04
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-13 22:34:49 +0600

    Update ManageUsers Page by Displaying List of Users and Implementing Update User Role Functionalities

--- src/pages/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import useAxios from '../../../../hooks/useAxios';
+import { useQuery } from '@tanstack/react-query';
+import { MdAddModerator, MdAdminPanelSettings } from "react-icons/md";
+import { PiStudentFill } from "react-icons/pi";
 
 const ManageUsers = () => {
+
+    const axios = useAxios();
+
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['applications'],
+        queryFn: async () => {
+            const res = await axios.get('/users');
+            return res.data;
+        }
+    });
+
+    const handleSetRole = (user, role) => {
+        const updatedRole = {
+            role: role
+        }
+
+        axios.patch(`/users/${user._id}`, updatedRole)
+            .then(res => {
+                refetch();
+                console.log('after status updating', res.data);
+            })
+    }
+
     return (
-        <div>
-            <h1>Manage Users Page form admin</h1>
+        <div className='md:max-w-7xl md:mx-auto p-4'>
+            <h1 className='text-2xl font-bold'>Manage Users</h1>
+            <h2 className='font-xl font-semibold mt-10'>Total Users ({users.length})</h2>
+
+            <div className="overflow-x-auto ">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Photo</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Set Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <td>{index + 1}</td>
+                                <td><img src={user?.photoURL} alt={user.displayName} className='w-8 h-auto' /></td>
+                                <td>{user.displayName}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role}</td>
+                                <td className='flex items-center gap-2'>
+                                    <button onClick={() => handleSetRole(user, 'admin')} className={`btn btn-sm btn-soft ${user.role === 'admin' && 'btn-secondary'}`}><MdAdminPanelSettings size={24} /></button>
+                                    <button onClick={() => handleSetRole(user, 'moderator')} className={`btn btn-sm btn-soft ${user.role === 'moderator' && 'btn-info'}`}><MdAddModerator size={19} /></button>
+                                    <button onClick={() => handleSetRole(user, 'student')} className={`btn btn-sm btn-soft ${user.role === 'student' && 'btn-success'}`}><PiStudentFill size={20} /></button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
