bf9b4eb697a6c3ca9b446f20b31cfe34b06284b0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-13 08:48:18 +0600

    Update Logo Component Text Color

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ import { GiGraduateCap } from "react-icons/gi";
 
 const Logo = () => {
     return (
-        <Link to={'/'} className='flex justify-center items-end gap-3'>
+        <Link to={'/'} className='flex justify-center items-end gap-3 text-orange-400'>
             {/* <img src={logoImg} className='w-14 h-auto' alt='logo image'></img> */}
             <GiGraduateCap size={40}/>
             <h1 className='text-2xl font-bold'>Scholars Stream</h1>
