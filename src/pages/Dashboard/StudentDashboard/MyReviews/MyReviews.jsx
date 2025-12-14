47063b5df852af79a999c69dd0064a49651af601
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-14 16:35:48 +0600

    Update My Review Page By Displaying Users Review Data

--- src/pages/Dashboard/StudentDashboard/MyReviews/MyReviews.jsx
+++ src/pages/Dashboard/StudentDashboard/MyReviews/MyReviews.jsx
@@ -16,11 +16,53 @@ const MyReviews = () => {
         }
     });
     refetch();
-    console.log('my reviews', my_reviews);
+
+    const deleteReview = review => {
+        axios.delete(`/review/${review._id}`)
+            .then(res => {
+                refetch();
+                console.log('delete scholarship success', res.data);
+            })
+    }
 
     return (
         <div className='md:max-w-7xl md:mx-auto p-4'>
-            <h1 className='font-xl font-semibold mt-10'>Total Reviews ({my_reviews.length})</h1>
+            <h1 className='text-2xl font-bold'>Total Reviews ({my_reviews.length})</h1>
+
+            <div className="overflow-x-auto ">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            {/* 
+                            // need to display info like this:
+
+                            <th>Scholarship Name</th>
+                            <th>University Name</th> */}
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Review</th>
+                            <th>Data</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            my_reviews.map((review, index) => <tr key={review._id}>
+                                <td>{index + 1}</td>
+                                <td>{review.reviewerName}</td>
+                                <td>{review.reviewerEmail}</td>
+                                <td>{review.review}</td>
+                                <td>{review.createdAt}</td>
+                                <td>
+                                     <button onClick={() => deleteReview(review)} className='btn btn-sm btn-soft btn-secondary w-25'>Delete</button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
