a5301ec6888efa5e07772bc2ca98f6f6f53d971e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-14 10:25:55 +0600

    Update AllScholarship Page by Implementing Pagination

--- src/pages/Root/All-Scholarships/AllScholarships.jsx
+++ src/pages/Root/All-Scholarships/AllScholarships.jsx
@@ -1,28 +1,119 @@
-import React from 'react';
-import useAxios from '../../../hooks/useAxios';
-import { useQuery } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
 import Scholarship from '../../../components/Scholarship';
 
 const AllScholarships = () => {
 
-    const axios = useAxios();
+    const [scholarships, setScholarships] = useState([]);
+    const [totalScholarships, setTotalScholarships] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [sort, setSort] = useState('size');
+    const [order, setOrder] = useState('');
+    const [searchText, setSearchText] = useState('');
+    const limit = 6
 
-    const { data: scholarships = [] } = useQuery({
-        queryKey: ['scholarship'],
-        queryFn: async () => {
-            const res = await axios.get('/scholarship');
-            return res.data;
-        }
-    })
+    useEffect(() => {
+        fetch(`http://localhost:3000/scholarship/?limit=${limit}&skip=${currentPage * limit}&sort=${sort}&order=${order}&search=${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+                setScholarships(data.scholarships);
+                setTotalScholarships(data.total);
+
+                const page = Math.ceil(totalScholarships / limit);
+                setTotalPage(page);
+            });
+    }, [totalScholarships, currentPage, sort, order, searchText]);
+
+    const handleSelect = (e) => {
+        const sortText = e.target.value;
+        setSort(sortText.split('-')[0]);
+        setOrder(sortText.split('-')[1]);
+    }
 
     return (
         <div className='my-6 md:max-w-7xl md:mx-auto p-4'>
-            <h1 className='md:text-2xl text-xl font-semibold mb-5'>All Scholarship({scholarships.length})</h1>
+            <div className="w-11/12 mx-auto flex flex-col-reverse lg:flex-row gap-5 items-start justify-between lg:items-end mt-10 mb-5">
+                <h1 className='md:text-2xl text-xl font-semibold'>All Scholarship({totalScholarships})</h1>
 
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                <form>
+                    <label className="input max-w-[300px] w-[300px] input-secondary">
+                        <svg
+                            className="h-[1em] opacity-50"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input onChange={(e) => setSearchText(e.target.value)} type="search" className="" placeholder="Search Scholarship" />
+                    </label>
+                </form>
+
+                <div className="">
+                    <select onChange={handleSelect} className="select">
+                        <option selected disabled={true}>
+                            Sort by <span className="text-xs">R / S / D</span>
+                        </option>
+                        <option value={"rating-desc"}>Ratings : High - Low</option>
+                        <option value={"rating-asc"}>Ratings : Low - High</option>
+                        <option value={"size-desc"}>Size : High - Low</option>
+                        <option value={"size-asc"}>Size : Low - High</option>
+                        <option value={"downloads-desc"}>Downloads : High - Low</option>
+                        <option value={"downloads-asc"}>Downloads : Low - High</option>
+                    </select>
+                </div>
+            </div>
+
+            <>
+                {/* Apps Grid */}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                    {scholarships.length === 0 ? (
+                        <div className="col-span-full text-center py-10 space-y-10">
+                            <h2 className="text-6xl font-semibold opacity-60">
+                                No Apps Found
+                            </h2>
+                            <button className="btn btn-primary">Show All Apps</button>
+                        </div>
+                    ) : (
+                        scholarships.map((scholarship) => <Scholarship key={scholarship.id} scholarship={scholarship}></Scholarship>)
+                    )}
+                </div>
+            </>
+
+            <div className="flex flex-wrap justify-center gap-3 py-10">
                 {
-                    scholarships.map(scholarship => <Scholarship key={scholarship._id} scholarship={scholarship}></Scholarship>)
+                    currentPage > 0 && <button
+                        onClick={() => setCurrentPage(currentPage - 1)}
+                        className="btn"
+                    >Prev
+                    </button>
                 }
+
+                {/* 0, 1, 2, 3, 4, 5, ... */}
+                {
+                    [...Array(totalPage).keys()].map(i => <button
+                        key={i}
+                        onClick={() => setCurrentPage(i)}
+                        className={`btn ${i === currentPage && 'btn-primary'}`}
+                    >{i + 1}</button>)
+                }
+
+                {
+                    currentPage < totalPage - 1 && <button
+                        onClick={() => setCurrentPage(currentPage + 1)}
+                        className="btn"
+                    >Next
+                    </button>
+                }
+
             </div>
         </div>
     );
