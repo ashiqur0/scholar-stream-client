02939a22b3e35a9b4db6a33c22a222f50d63b5da
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-14 07:37:36 +0600

    Update FAQ Section of Home Page

--- src/pages/Root/Home/FAQ/FAQ.jsx
+++ src/pages/Root/Home/FAQ/FAQ.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { RiArrowDropUpLine } from "react-icons/ri";
 
 const FAQ = () => {
+
+    const [faqs, setFAQs] = useState([]);
+    const [seeMore, setSeeMore] = useState(false);
+
+    useEffect(() => {
+        fetch('/faq.json')
+            .then(res => res.json())
+            .then(data => setFAQs(data));
+    }, [])
+
+    const handleFAQ = (id) => {
+        const updated = faqs.map(faq =>
+            faq.id === id
+                ? { ...faq, open: !faq.open }
+                : { ...faq, open: false }
+        );
+
+        setFAQs(updated);
+    };
+
     return (
-        <div className='md:max-w-7xl md:mx-auto p-4'>
-            <h1>FAQ Section For Main Home Page</h1>
+        <div className='my-20 md:max-w-275 md:mx-auto mx-4'>
+            <h1 className='md:text-3xl text-2xl font-extrabold text-secondary text-center'>Frequently Asked Question (FAQ)</h1>
+            <p className='text-txt text-center max-w-200 mt-3 mx-auto'>Enhance posture, mobility, and well-being effortlessly with Posture Pro. Achieve proper alignment, reduce pain, and strengthen your body with ease!</p>
+
+            <div className='mt-5'>
+                {
+                    (!seeMore ? faqs.slice(0, 5) : faqs).map(faq => (
+                        <div key={faq.id}
+                            onClick={() => {
+                                !faq.open && handleFAQ(faq.id);
+                            }}
+                            className=' mb-5 p-4 rounded-xl'>
+
+                            <div className='flex items-center justify-between'>
+                                <div>
+                                    <h1 className='text-xl font-bold'>{faq.question}</h1>
+                                    <p className={`mt-2 ${!faq.open && "hidden" || "flex"}`}>{faq.answer}</p>
+                                </div>
+                                <div>
+                                    {
+                                        faq.open ? <RiArrowDropUpLine size={30} /> : <RiArrowDropDownLine size={30} />
+                                    }
+                                </div>
+                            </div>
+                        </div>))
+                }
+            </div>
+
+            <button onClick={() => setSeeMore(!seeMore)}
+                className='btn bg-primary text-[1rem] font-bold rounded-xl'>{seeMore ? "See Less FAQ's" : "See More FAQ's"}</button>
         </div>
     );
 };
