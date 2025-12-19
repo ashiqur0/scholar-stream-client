3f1d62c3b0076334b8720108646970366a7a7bf7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-19 20:58:51 +0600

    Update Student DashboardHome Page

--- src/pages/Dashboard/Home/StudentDashboardHome.jsx
+++ src/pages/Dashboard/Home/StudentDashboardHome.jsx
@@ -7,9 +7,9 @@ const StudentDashboardHome = () => {
 
     return (
         <div className='md:max-w-7xl md:mx-auto p-4 items-center'>
-            <h1 className='md:text-7xl font-extrabold text-center mt-10'>Hello, <span className='text-orange-400'>{user.displayName}</span></h1>
+            <h1 className='md:text-7xl text-5xl font-extrabold text-center mt-10'>Hello, <span className='text-orange-400'>{user.displayName}</span></h1>
 
-            <h1 className='text-4xl text-center mt-5'>Welcome You To Student Dashboard Home Page</h1>
+            <h1 className='md:text-4xl text-2xl text-center mt-5'>Welcome You To Student Dashboard Home Page</h1>
         </div>
     );
 };
