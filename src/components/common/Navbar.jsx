a67529fbb2e5742a27d431d60e4b1f0b194640fb
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-19 22:58:05 +0600

    Bug Fix: Logo Now Full Width In Small Devices

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
     return (
         <nav className='md:max-w-7xl md:mx-auto pt-4'>
             <div className="navbar ">
-                <div className="navbar-start">
+                <div className="navbar-start w-full">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -45,7 +45,7 @@ const Navbar = () => {
                         {links}
                     </ul>
                 </div>
-                <div className="md:flex hidden gap-3 navbar-end">
+                <div className="md:flex hidden gap-3 navbar-end w-full">
                     <DarkLightThemeToggle />
                     {
                         user ? <>
