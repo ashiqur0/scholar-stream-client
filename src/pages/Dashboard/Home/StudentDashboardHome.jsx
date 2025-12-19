4645b41d55878ef3c98287285d6d5ba1d96f7777
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-19 20:54:19 +0600

    Update Student DashboardHome Page

--- src/pages/Dashboard/Home/StudentDashboardHome.jsx
+++ src/pages/Dashboard/Home/StudentDashboardHome.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const StudentDashboardHome = () => {
+
+    const { user } = useAuth();
+
     return (
-        <div>
-            <h1>Student Dashboard Home</h1>
+        <div className='md:max-w-7xl md:mx-auto p-4 items-center'>
+            <h1 className='md:text-7xl font-extrabold text-center mt-10'>Hello, <span className='text-orange-400'>{user.displayName}</span></h1>
+
+            <h1 className='text-4xl text-center mt-5'>Welcome You To Student Dashboard Home Page</h1>
         </div>
     );
 };
