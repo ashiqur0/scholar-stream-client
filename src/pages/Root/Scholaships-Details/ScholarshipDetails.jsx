a2874b553ab5506d5900bcf9f023ad914a93bffe
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-21 23:18:59 +0600

    Update Application Form

--- src/pages/Root/Scholaships-Details/ScholarshipDetails.jsx
+++ src/pages/Root/Scholaships-Details/ScholarshipDetails.jsx
@@ -115,15 +115,6 @@ const ScholarshipDetails = () => {
                                 defaultValue={universityName}
                             />
 
-                            {/* University Address */}
-                            <label className="label mt-4 text-[14px]">University Address</label>
-                            <input
-                                type="text"
-                                {...register('universityAddress')}
-                                className="input w-full"
-                                defaultValue={universityAddress}
-                            />
-
                             {/* Applicant Name */}
                             <label className="label mt-4 text-[14px]">Applicant Name</label>
                             <input
@@ -151,6 +142,15 @@ const ScholarshipDetails = () => {
                                 {...register('scholarshipCategory')}
                                 className="input w-full"
                                 defaultValue={scholarshipCategory}
+                            />
+
+                            {/* Subject Category */}
+                            <label className="label mt-4 text-[14px]">Subject Category</label>
+                            <input
+                                type="text"
+                                {...register('subjectCategory')}
+                                className="input w-full"
+                                defaultValue={subjectCategory}
                             />
 
                             {/* degree */}
