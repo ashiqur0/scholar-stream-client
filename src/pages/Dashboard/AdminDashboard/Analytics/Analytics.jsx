bafa078e01f48cb025d942c794af7506cdea97db
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-22 09:34:37 +0600

    Update Layout And Styles In Analytics Page

--- src/pages/Dashboard/AdminDashboard/Analytics/Analytics.jsx
+++ src/pages/Dashboard/AdminDashboard/Analytics/Analytics.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import { Legend, Pie, PieChart, Tooltip } from 'recharts';
+import { FaChevronRight } from 'react-icons/fa6';
 
 const Analytics = () => {
 
@@ -42,27 +43,38 @@ const Analytics = () => {
             <title>Analytics</title>
             <h1 className='text-2xl font-bold'>Analytics of Application Status</h1>
 
-            <div className="stats shadow">
-                <div className="stat mt-10">
-                    <div className="stat-title text-2xl">Total Fees Collected</div>
-                    <div className="stat-value">${summary.totalFees}</div>
-                </div>
+            <div className="overflow-x-auto">
+                <div className="stats shadow min-w-[800px] w-full">
+                    <div className="stat mt-10">
+                        <div className="stat-title text-2xl">Total Fees Collected</div>
+                        <div className="stat-value flex justify-between items-center ">
+                            <span>{summary.totalFees}</span>
+                            <span><FaChevronRight size={20} /></span>
+                        </div>
+                    </div>
 
-                <div className="stat mt-10">
-                    <div className="stat-title text-2xl">Total Users</div>
-                    <div className="stat-value">{summary.totalUser}</div>
-                </div>
+                    <div className="stat mt-10">
+                        <div className="stat-title text-2xl">Total Users</div>
+                        <div className="stat-value flex justify-between items-center ">
+                            <span>{summary.totalUser}</span>
+                            <span><FaChevronRight size={20} /></span>
+                        </div>
+                    </div>
 
-                <div className="stat mt-10">
-                    <div className="stat-title text-2xl">Total Scholarships</div>
-                    <div className="stat-value">{summary.totalScholarship}</div>
+                    <div className="stat mt-10">
+                        <div className="stat-title text-2xl">Total Scholarships</div>
+                        <div className="stat-value flex justify-between items-center ">
+                            <span>{summary.totalScholarship}</span>
+                            <span><FaChevronRight size={20} /></span>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <div className='mt-10 flex items-center gap-10'>
+            <div className='mt-10 flex md:flex-row flex-col items-center gap-10'>
                 <div className='w-full h-[400px]'>
                     <h1 className=' text-2xl font-bold'>Application Per University</h1>
-                    <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 2 }} responsive>
+                    <PieChart style={{ width: '100%', maxWidth: '600px', maxHeight: '260vh', aspectRatio: 2 }} responsive>
                         <Pie
                             dataKey="value"
                             startAngle={180}
